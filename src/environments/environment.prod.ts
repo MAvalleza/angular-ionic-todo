efbf895e82a8db4b69cf08616731be8161d626b8
Marc Almond Valleza <majvalleza@gmail.com>
2022-02-01 23:42:05 +0800

    chore: add firebase config in env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA_x_CwziAJ-7iKSoG_Mhy3-sgMPXRu0nA",
+    authDomain: "angular-ionic-todo.firebaseapp.com",
+    projectId: "angular-ionic-todo",
+    storageBucket: "angular-ionic-todo.appspot.com",
+    messagingSenderId: "232278112504",
+    appId: "1:232278112504:web:e8235f4e3a6803f8fea30d"
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA_x_CwziAJ-7iKSoG_Mhy3-sgMPXRu0nA",
+    authDomain: "angular-ionic-todo.firebaseapp.com",
+    projectId: "angular-ionic-todo",
+    storageBucket: "angular-ionic-todo.appspot.com",
+    messagingSenderId: "232278112504",
+    appId: "1:232278112504:web:e8235f4e3a6803f8fea30d"
+  },
 };
 
 /*
